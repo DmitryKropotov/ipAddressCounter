d0dfb14bf5444bf1e70adb04d6f1e2cef0df2eed
kropo <kropotovd@gmail.com>
2024-07-14 14:49:31 +0200

    working with big files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,10 @@
 package org.example;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.ref.Cleaner;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.util.Arrays;
@@ -12,25 +14,27 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
         String filePath = "file.txt";
-        for (int i = 2; i<100; i++) {
-            long uniqueAddressesCount = countUniqueIPv4Addresses(filePath, i);
-            System.out.println("Number of unique IPv4 addresses: " + uniqueAddressesCount + " i is " + i);
-        }
+        long uniqueAddressesCount = countUniqueIPv4Addresses(filePath);
+        System.out.println("Number of unique IPv4 addresses: " + uniqueAddressesCount);
     }
 
-    public static long countUniqueIPv4Addresses(String filePath,  final int SIZE_TO_READ_SIMULTANEOUSLY) {
+    public static long countUniqueIPv4Addresses(String filePath) {
         Set<Integer> uniqueAddresses = new HashSet<>();
-        //final int SIZE_TO_READ_SIMULTANEOUSLY = 8;
+        File file = new File(filePath);
+        long fileSizeInBytes = file.length();
+        final int SIZE_TO_READ_SIMULTANEOUSLY = (int)(Math.min(fileSizeInBytes, Math.pow(2, 15)));
         byte[] buffer = new byte[SIZE_TO_READ_SIMULTANEOUSLY]; // Adjust buffer size as needed
 
         try (InputStream inputStream = new FileInputStream(filePath)) {
             int bytesRead;
             String savedBuffer = "";
+            Runtime runtime = Runtime.getRuntime();
             while ((bytesRead = inputStream.read(buffer)) != -1) {
                 String[] bufferedVal = Arrays.stream(new String(buffer, 0, bytesRead).
-                        split("\n")).filter(st->!st.isEmpty()).toArray(String[]::new);
+                        split("\n")).
+                        filter(st->!st.isEmpty()).
+                        toArray(String[]::new);
                 if (buffer[0] == 10 && !savedBuffer.isEmpty()) {
-                    //System.out.println("buffer[0] == 10 " + savedBuffer);
                     processLine(savedBuffer, uniqueAddresses);
                 } else {
                     bufferedVal[0] = savedBuffer + bufferedVal[0];
@@ -42,12 +46,11 @@ public class Main {
                 for (int i = 0; i<bufferedVal.length-(bytesRead<SIZE_TO_READ_SIMULTANEOUSLY ||
                         buffer[SIZE_TO_READ_SIMULTANEOUSLY - 1] == 10  ||
                         buffer[SIZE_TO_READ_SIMULTANEOUSLY-1] == 13? 0: 1); i++) {
-                    //System.out.println("i=" + i + " " + savedBuffer);
                     processLine(bufferedVal[i], uniqueAddresses);
+                    prepareObjectForCleaning(bufferedVal[i]);
                 }
             }
             if(!savedBuffer.isEmpty()) {
-                //System.out.println("savedBuffer is not empty " + savedBuffer);
                 processLine(savedBuffer, uniqueAddresses);
             }
         } catch (IOException e) {
@@ -58,7 +61,6 @@ public class Main {
     }
 
     private static void processLine(String line, Set<Integer> uniqueAddresses) {
-        System.out.println("line is " + line);
         try {
             InetAddress inetAddress = InetAddress.getByName(line.trim());
             if (inetAddress instanceof Inet4Address) {
@@ -75,6 +77,11 @@ public class Main {
                 (bytes[1] & 0xFF) << 16 |
                 (bytes[2] & 0xFF) << 8 |
                 (bytes[3] & 0xFF);
+    }
+
+    private static void prepareObjectForCleaning(Object obj) {
+        Cleaner cleaner = Cleaner.create();
+        cleaner.register(obj, () -> {});
     }
 
 }
